74bb81e1e5d5994be8bb294286c6aefcf77924f2
Ganesh Lohakare <ganeshlohakare329@gmail.com>
2026-07-01 18:00:44 +0530

    Scope difined to the bean PaymentService

--- src/main/java/com/sk/App.java
+++ src/main/java/com/sk/App.java
@@ -14,6 +14,15 @@ public class App
         System.err.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("Sample.xml");
         PaymentService service = context.getBean(PaymentService.class);
+        PaymentService service1 = context.getBean(PaymentService.class);
+        System.out.println(service.hashCode()); // 1043208434
+        System.out.println(service1.hashCode()); // 1043208434
+        
+        // both object having same hashcodes due to scope is by default is singleType 
+        // if you want to create every time new object then you need to add property as scope = "prototype" while initializing bean
+        // then you will get 1043208434  1192171522 two different objects 
+        
+        
         service.doPayment(1000);
     }
 }
